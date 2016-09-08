3d478e3340227b5eb6def188e56c8e4331321509
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-09-08 15:17:13 +0200

    Replace heart icon with pig

--- resources/views/widgets/like.blade.php
+++ resources/views/widgets/like.blade.php
@@ -5,7 +5,7 @@
     <input type="hidden" name="payload" value="{{ $item->getPayload() }}">
 
     <button type="submit" class="action__like {{ $item->liked() ? 'action__like--active' : null }}">
-        <i class="fa fa-heart"></i>
+        <img src="{{ asset('img/pig.svg') }}" width="16" alt="sneefR" class="hero__img">
         <span class="js-like-text"
               data-unlike-text="@lang('dashboard.activity.unlike')"
               data-like-text="@lang('dashboard.activity.like')">
