041d149d1410fd46b14afb18c1ef9b8f26c9ce2f
Bilel Dhaouadi <dhaouadi.bil@gmail.com>
2016-09-08 17:39:33 +0100

    Separate avatar from settings dropdown (#12)
    * Distinct avatar from settings dropdown
    
    * Add a test for avatar and settings dropdown
    
    * Display retina logo in navbar
    
    * Put the correct role attributes on menu items
    
    * Display retina avatar when needed
    
    * Update tests to be more readable

--- resources/views/partials/avatar.blade.php
+++ resources/views/partials/avatar.blade.php
@@ -6,16 +6,18 @@ if ($of instanceof \Sneefr\Models\Shop) {
 
     $route = route('shops.show', $of);
     $src = $of->getLogo($size);
+    $src2x = $of->getLogo($size . '@2x');
     $name = $of->getName();
 
 } else {
 
     $route = route('profiles.ads.index', $of);
     $src = \Img::avatar($of->facebook_id, $size);
+    $src2x = \Img::avatar($of->facebook_id, $size . '@2x');
     $name = $of->present()->fullName();
 
 }
 ?>
 <a class="card__avatar" href="{{ $route }}" title="{{ $name }}">
-    <img class="card__image" src="{{ $src }}" alt="{{ $name }}" height="{{ $height }}" width="{{ $width }}">
+    <img class="card__image" src="{{ $src }}" alt="{{ $name }}" srcset="{{ $src2x }} 2x" height="{{ $height }}" width="{{ $width }}">
 </a>
