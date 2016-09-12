42c27a4258d307a3f6181d10e5ad1d02466ea725
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-09-12 15:39:55 +0200

    Inject billing interfacte in method

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -1,13 +1,17 @@
 <?php namespace Sneefr\Http\Controllers;
 
+use Sneefr\Contracts\BillingInterface;
+
 class SettingsController extends Controller
 {
     /**
      * Display settings of this user.
      *
+     * @param \Sneefr\Contracts\BillingInterface $billing
+     *
      * @return \Illuminate\View\View
      */
-    public function show()
+    public function show(BillingInterface $billing)
     {
         return view('me.show');
     }
