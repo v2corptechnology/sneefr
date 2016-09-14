76b59142f10076c5c8b7326375e15835b35cec5b
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-09-14 14:18:57 +0200

    Add route to create an item

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sneefr\Http\Controllers;
+
+use Sneefr\Http\Requests;
+
+class ItemsController extends Controller
+{
+    /**
+     * Display the form to create a new item.
+     */
+    public function create()
+    {
+    }
+}
