32fb0d45da9b681ea65ecfd6133da8594d1ad298
Bilel Dhaouadi <dhaouadi.bil@gmail.com>
2016-09-15 15:53:43 +0100

    Add a test for register/login with activation

--- tests/features/ViewLoginRegisterTest.php
+++ tests/features/ViewLoginRegisterTest.php
@@ -5,13 +5,40 @@ class ViewLoginRegisterTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function test_view_login_page()
+    public function test_user_register_for_an_account_via_email_with_activation()
     {
-        $pages = ["/login", "/register", "/password/reset"];
+        factory(\Sneefr\Models\Ad::class, 3)->create();
 
-        foreach ($pages as $page)
-        {
-            $this->visit($page);
-        }
+        $this->visit('/register')
+             ->type('email@sneefr.com', 'email')
+             ->type('password', 'password')
+             ->type('password', 'password_confirmation')
+             ->press(trans('button.register'));
+
+        $this->see(trans('auth.activation'))
+            ->seeInDatabase('users', ["email" => "email@sneefr.com", "verified" => 0]);
+
+        $user = \Sneefr\Models\User::whereEmail('email@sneefr.com')->firstOrFail();
+
+        $key = encrypt(['id' => $user->getId(), 'email' => $user->getEmail()]);
+
+        $this->visit("/register/activation/" . $key);
+
+        $this->see(trans('feedback.email_activation_success'))
+            ->seeInDatabase('users', ["email" => "email@sneefr.com", "verified" => 1]);
     }
+
+    public function test_login_user_with_not_activated_account()
+    {
+        $user = factory(\Sneefr\Models\User::class)->create(['verified' => 0, 'password' => bcrypt('password')]);
+
+        $this->visit('/login')
+            ->type($user->getEmail(), 'email')
+            ->type('password', 'password')
+            ->press(trans('button.login'));
+
+        $this->see(trans('feedback.account_not_activated'));
+    }
+
+
 }
