89efd17537b9f6b9f86dec40436efa06ca5041bc
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-09-16 10:13:54 +0200

    Update transaction tables

--- database/migrations/2016_08_18_130616_create_transactions_table.php
+++ database/migrations/2016_08_18_130616_create_transactions_table.php
@@ -15,26 +15,18 @@ class CreateTransactionsTable extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('ad_id');
-
-            if(env('DB_CONNECTION') == 'sqlite') {
-                $table->unsignedInteger('buyer_id')->nullable()->after('ad_id');
-                $table->unsignedInteger('seller_id')->nullable()->after('buyer_id');
-                $table->json('stripe_data')->nullable()->after('seller_id');
-                $table->json('details')->nullable()->after('stripe_data');
-
-                $table->foreign('buyer_id')->references('id')->on('users');
-                $table->foreign('seller_id')->references('id')->on('users');
-            } else {
-                $table->unsignedInteger('user_id');
-                $table->text('data');
-                $table->foreign('user_id')->references('id')->on('users');
-            }
-
+            $table->unsignedInteger('buyer_id')->nullable();
+            $table->unsignedInteger('seller_id')->nullable();
+            $table->json('stripe_data')->nullable();
+            $table->json('details')->nullable();
             // Define timestamps to record dates and times of changes.
             $table->timestamps();
 
             // Indices and foreign keys
             $table->foreign('ad_id')->references('id')->on('ads');
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('seller_id')->references('id')->on('users');
+
         });
     }
 
