ccf918c3c7da379efeb4f6278a45cb9b68b5fbcf
Bilel Dhaouadi <dhaouadi.bil@gmail.com>
2016-09-24 21:36:34 +0100

    Add responsive style

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,7 +6,7 @@
             background-color: #ff3d7b;
             background-image: url('{{ url("/img/shop-background.png") }}');
             background-repeat: no-repeat;
-            background-position: center;
+            background-position: center bottom;
             background-size: 100%;
             height: 380px;
             padding-top: 50px;
@@ -26,7 +26,7 @@
                     <div class="home__header__content">
                         <h1 class="home__header__title">Buy from great local trusted shops in your city, all in one place</h1>
                         <p class="home__header__dscription">Search what you want, sneefR shows you the shops around you that sell what you’re looking for at the best price. Buy directly online and get delivered or go instore to pickup your item for free.</p>
-                        <span><i class="fa fa-map-marker"></i> Los Angeles - More city to come</span>
+                        <span class="home__header__locale"><i class="fa fa-map-marker"></i> Los Angeles - More city to come</span>
                     </div>
                 </div>
 
