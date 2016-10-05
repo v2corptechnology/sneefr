713cbad82b5d3a0a4fc8c6fddc1ffafeb38ec239
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-05 17:33:11 +0200

    Add indentation

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,41 +1,40 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row">
+    <div class="container">
+        <div class="row">
 
-        {{-- Trending shops --}}
-        <div class="col-sm-9">
+            {{-- Trending shops --}}
+            <div class="col-sm-9">
 
-            @include('pages.home.quote')
+                @include('pages.home.quote')
 
-            <div class="row">
+                <div class="row">
 
-                @include('pages.home.trending-shops', ['shops' => $topShops])
+                    @include('pages.home.trending-shops', ['shops' => $topShops])
+
+                </div>
+            </div>
+
+            {{-- Best selling --}}
+            <div class="col-sm-3 ">
+
+                @include('pages.home.best-selling', ['items' => $bestSellers])
 
             </div>
         </div>
 
-        {{-- Best selling --}}
-        <div class="col-sm-3 ">
+        <div class="row" id="shop-categories">
 
-            @include('pages.home.best-selling', ['items' => $bestSellers])
+            @include('pages.home.shops-by-category', ['categories' => $categories, 'shops' => $shopsByCategory])
 
         </div>
-    </div>
-
-    <div class="row" id="shop-categories">
-
-        @include('pages.home.shops-by-category', ['categories' => $categories, 'shops' => $shopsByCategory])
 
     </div>
 
-</div>
+    <div class="shop__pub text-center color-white">
 
-<div class="shop__pub text-center color-white">
+        @include('pages.home.shop-advertising')
 
-    @include('pages.home.shop-advertising')
-
-</div>
-
+    </div>
 @endsection
