efd43fb0002f8b418bbb186ef58ebd7ecf2b1316
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-05 17:24:01 +0200

    Simplify the markup

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -25,60 +25,55 @@
     </div>
 
     <div class="row" id="shop-categories">
-
         <div class="col-sm-12">
             <div class="home__section home__section--padding">
                 <h4 class="home__section__title">@lang('common.browse_by_category')</h4>
             </div>
         </div>
 
-        <div class="row">
-            {{-- categpries --}}
-            <div class="col-sm-2">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <ul class="home__categories">
-                            <li>
-                                <a href="{{ route('home') }}#shop-categories">All</a>
+        {{-- categpries --}}
+        <div class="col-sm-2">
+            <div class="row">
+                <div class="col-xs-12">
+                    <ul class="home__categories">
+                        <li>
+                            <a href="{{ route('home') }}#shop-categories">All</a>
+                        </li>
+                        @foreach($categories as $category)
+                            <li class="home__categories__item{{ $categories->parent == $category->id ? '--active' : '' }}">
+                                <a href="{{ route('home', "category={$category->id}#shop-categories") }}">{{ trans("category.{$category->id}") }}</a>
                             </li>
-                            @foreach($categories as $category)
-                                <li class="home__categories__item{{ $categories->parent == $category->id ? '--active' : '' }}">
-                                    <a href="{{ route('home', "category={$category->id}#shop-categories") }}">{{ trans("category.{$category->id}") }}</a>
-                                </li>
-                                @if( $categories->parent == $category->id && $category->childrens)
-                                    <ul class="home__categories home__categories--child">
-                                        @foreach($category->childrens as $child)
-                                            <li class="home__categories__item{{ $categories->child == $child->id ? '--active' : '' }}">
-                                                @if($categories->child == $child->id)
-                                                    <i class="fa fa-check color-pink home__categories__item--checked"></i>
-                                                @endif
-                                                <a href="{{ route('home', "category={$child->id}") }}#shop-categories">{{ trans("category.{$child->id}") }}</a>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                @endif
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            {{-- ads by categorie  --}}
-            <div class="col-sm-10">
-                <div class="row">
-                    <div class="col-sm-12">
-                        @foreach($shopsByCategory as $ad)
-                            <div class="col-sm-4">
-                                @include('partials.card', ['item' => $ad, 'multiple' => false])
-                            </div>
+                            @if( $categories->parent == $category->id && $category->childrens)
+                                <ul class="home__categories home__categories--child">
+                                    @foreach($category->childrens as $child)
+                                        <li class="home__categories__item{{ $categories->child == $child->id ? '--active' : '' }}">
+                                            @if($categories->child == $child->id)
+                                                <i class="fa fa-check color-pink home__categories__item--checked"></i>
+                                            @endif
+                                            <a href="{{ route('home', "category={$child->id}") }}#shop-categories">{{ trans("category.{$child->id}") }}</a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            @endif
                         @endforeach
-                    </div>
-                    <div class="col-sm-12" style="padding-right: 30px;">
-                        <a href="{{ route('search.index', ['type' => 'shop']) }}" class="btn btn-default-o pull-right">@lang('button.see_more')</a>
-                    </div>
+                    </ul>
                 </div>
-
             </div>
-            {{-- end ads by categorie  --}}
+        </div>
+        {{-- ads by categorie  --}}
+        <div class="col-sm-10">
+            <div class="row">
+                <div class="col-sm-12">
+                    @foreach($shopsByCategory as $ad)
+                        <div class="col-sm-4">
+                            @include('partials.card', ['item' => $ad, 'multiple' => false])
+                        </div>
+                    @endforeach
+                </div>
+                <div class="col-sm-12" style="padding-right: 30px;">
+                    <a href="{{ route('search.index', ['type' => 'shop']) }}" class="btn btn-default-o pull-right">@lang('button.see_more')</a>
+                </div>
+            </div>
         </div>
     </div>
 
