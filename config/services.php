96f1e6fb3a45f43f7fb625b11190e4840cf38dfb
Bilel Dhaouadi <dhaouadi.bil@gmail.com>
2016-10-06 11:36:44 +0100

    Update facebook auth to use Socialite

--- config/services.php
+++ config/services.php
@@ -39,6 +39,7 @@ return [
     'facebook' => [
         'client_id'     => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect'      => env('FACEBOOK_CAllBACK'),
     ],
 
     'rollbar' => [
--- resources/lang/en/login.php
+++ resources/lang/en/login.php
@@ -3,4 +3,5 @@
 return [
     'btn_facebook_login' => "Login with Facebook",
     'relogging'          => "Relogging",
+    'account_by_fb_exist'    => "An account exist with this email not linked to your Facebook account.",
 ];
