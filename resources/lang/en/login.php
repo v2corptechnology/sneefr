865586bb10fba0e72ca50457d485868f0f9167f7
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-06 17:21:37 +0200

    Remove missing scopes error

--- resources/lang/en/login.php
+++ resources/lang/en/login.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'btn_facebook_login' => "Login with Facebook",
-    'relogging'          => "Relogging",
-    'account_by_fb_exist'    => "An account exist with this email not linked to your Facebook account.",
+    'btn_facebook_login'  => "Login with Facebook",
+    'account_by_fb_exist' => "An account exist with this email not linked to your Facebook account.",
 ];
