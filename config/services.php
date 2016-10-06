0dd339e6dccb9be7015cb803cea9ce47cc38cb91
Bilel Dhaouadi <dhaouadi.bil@gmail.com>
2016-10-06 15:34:09 +0100

    Update facebook callback env variable name

--- config/services.php
+++ config/services.php
@@ -39,7 +39,7 @@ return [
     'facebook' => [
         'client_id'     => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect'      => env('FACEBOOK_CAllBACK'),
+        'redirect'      => env('FACEBOOK_CALLBACK'),
     ],
 
     'rollbar' => [
