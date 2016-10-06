e083590eb19dba0ee19fa80870d669bec87e5915
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-06 11:52:08 +0200

    Make ads title clickable

--- resources/views/pages/home/best-selling.blade.php
+++ resources/views/pages/home/best-selling.blade.php
@@ -7,19 +7,23 @@
             <div class="card__box__footer">
                 <div class="card__box__avatar">
                     <a href="{{ route('ad.show', $item) }}">
-                        <img class="card__box__avatar__img" src="{{ $item->images('60x60', true)[0] }}" alt="">
+                        <img class="card__box__avatar__img"
+                             src="{{ $item->images('60x60', true)[0] }}" alt="">
                     </a>
                 </div>
-                <div class="card__box__description">
-                    <span class="text-indent">{{ $item->present()->title() }}</span>
-                    <div>
-                        <span class="text-primary">{!! $item->present()->price() !!}</span>
+                <a href="{{ route('ad.show', $item) }}">
+                    <div class="card__box__description">
+                        <span class="is-truncated">{{ $item->present()->title() }}</span>
+                        <div>
+                            <span class="text-primary">{!! $item->present()->price() !!}</span>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
         @endforeach
     </div>
     <div class="text-center">
-        <a class="btn btn-default-o" href="{{ route('search.index', ['type' => 'ad']) }}">@lang('button.see_more')</a>
+        <a class="btn btn-default-o"
+           href="{{ route('search.index', ['type' => 'ad']) }}">@lang('button.see_more')</a>
     </div>
 </div>
