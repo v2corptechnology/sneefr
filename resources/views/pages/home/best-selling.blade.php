4e620bdea64025ddb1d022ed4398b8ea65efa063
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-06 12:50:14 +0200

    Simplify best selling markup and styling

--- resources/views/pages/home/best-selling.blade.php
+++ resources/views/pages/home/best-selling.blade.php
@@ -1,29 +1,29 @@
-<div class="box__panel box__panel--padding">
-    <div class="home__section">
-        <h4 class="home__section__title home__section__title--small">Best Seller</h4>
+<div class="box__panel">
+
+    <div class="box__panel--padding">
+        <div class="home__section">
+            <h4 class="home__section__title home__section__title--small">Best Seller</h4>
+        </div>
     </div>
-    <div class="row">
-        @foreach($items as $item)
-            <div class="card__box__footer">
-                <div class="card__box__avatar">
-                    <a href="{{ route('ad.show', $item) }}">
-                        <img class="card__box__avatar__img"
-                             src="{{ $item->images('60x60', true)[0] }}" alt="">
-                    </a>
-                </div>
-                <a href="{{ route('ad.show', $item) }}">
-                    <div class="card__box__description">
-                        <span class="is-truncated">{{ $item->present()->title() }}</span>
-                        <div>
-                            <span class="text-primary">{!! $item->present()->price() !!}</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        @endforeach
-    </div>
+
+    @foreach ($items as $item)
+        <div class="row best-selling">
+            <a class="best-selling__item" href="{{ route('ad.show', $item) }}">
+                <span class="col-xs-4 is-narrow-pr">
+                    <img class="img-responsive img-rounded" src="{{ $item->images('60x60', true)[0] }}"
+                         alt="{{ $item->present()->title() }}">
+                </span>
+                <span class="col-xs-8 is-narrow-pl">
+                    <h1 class="best-selling__heading is-truncated">{{ $item->present()->title() }}</h1>
+                    <p>{!! $item->present()->price() !!}</p>
+                </span>
+            </a>
+        </div>
+    @endforeach
+
     <div class="text-center">
         <a class="btn btn-default-o"
            href="{{ route('search.index', ['type' => 'ad']) }}">@lang('button.see_more')</a>
     </div>
+
 </div>
