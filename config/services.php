a2d0c57e0e0c3b77ce48b5a9d1a082bef362c6f7
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-07 10:11:55 +0200

    Remove need of .env variable for FB redirect

--- config/services.php
+++ config/services.php
@@ -39,7 +39,8 @@ return [
     'facebook' => [
         'client_id'     => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect'      => env('FACEBOOK_CALLBACK'),
+        'redirect'      => env('APP_URL') . '/auth/callback',
+
     ],
 
     'rollbar' => [
