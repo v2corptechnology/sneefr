ff7f3a51d1eecec9beddac431a30edc78ac0f4fa
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-18 17:14:43 +0200

    Add relationships to Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -19,4 +19,34 @@ class Transaction extends Model
      * @var array
      */
     protected $casts = ['stripe_data' => 'array', 'details' => 'array'];
+
+    /**
+     * The ad relationship of the transaction.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function ad()
+    {
+        return $this->belongsTo(Ad::class)->withTrashed();
+    }
+
+    /**
+     * The seller relationship of the transaction.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function seller()
+    {
+        return $this->belongsTo(User::class)->withTrashed();
+    }
+
+    /**
+     * The buyer relationship of the transaction.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function buyer()
+    {
+        return $this->belongsTo(User::class)->withTrashed();
+    }
 }
