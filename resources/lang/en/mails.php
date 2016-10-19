93e0fdd075bed50a829efa62658b94303dde1c69
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-19 18:12:53 +0200

    Configure the email

--- resources/lang/en/mails.php
+++ resources/lang/en/mails.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+    'message' => [
+        'inbox_title' => "You have a question about one item",
+        'title'       => ":name just asked you a question about \":title\"",
+        'content'     => ":message",
+        'reply'       => ":name, is already the recipient of this email, just \"reply\" as usual. (or send an email to :email).",
+    ],
+
     'purchased' => [
         'inbox_title'        => "You just bought something on Sneefr",
         'title'              => "You just bought \":item\" :price | You just bought :nb \":item\" :price",
