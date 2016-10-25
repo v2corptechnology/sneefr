e3510fea86cb13bb800c8518988f60020d588da5
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-25 15:00:39 +0200

    Tags are timestamped now

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var bool
-     */
-    public $timestamps = false;
-
-    /**
      * The attributes that should be cast to native types.
      *
      * @var array
