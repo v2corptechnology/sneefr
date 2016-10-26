98e682f98c388bba91a5158149d937d2e3d082a7
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-26 12:16:25 +0200

    Allow user to leave an evaluation

--- app/Policies/ShopPolicy.php
+++ app/Policies/ShopPolicy.php
@@ -3,6 +3,8 @@
 namespace Sneefr\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Sneefr\Models\Ad;
+use Sneefr\Models\Evaluation;
 use Sneefr\Models\Shop;
 use Sneefr\Models\User;
 
@@ -50,6 +52,32 @@ class ShopPolicy
     public function destroy(User $user, Shop $shop)
     {
         return $this->isShopOwner($user, $shop) && $user->isAdmin() && ! app()->environment('local', 'staging');
+    }
+
+    /**
+     * Check if the current user can evaluate this shop
+     * for a specific item
+     *
+     * @param \Sneefr\Models\User $user
+     * @param \Sneefr\Models\Shop $shop
+     * @param \Sneefr\Models\Ad $ad
+     *
+     * @return bool
+     */
+    public function evaluate(User $user, Shop $shop, Ad $ad)
+    {
+        // Is there a pending evaluation for this shop ?
+        $evaluation = Evaluation::pending()
+            ->where('evaluator_id', $user->getId())
+            ->where('shop_id', $shop->getId())
+            ->where('ad_id', $ad->getId())
+            ->first();
+
+        if (! $evaluation) {
+            abort(403, "Either this evaluation expired either you are not authorized to review this shop");
+        }
+
+        return true;
     }
 
     private function isShopOwner(User $user, Shop $shop) : bool
