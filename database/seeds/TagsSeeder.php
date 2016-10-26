b9d4e49c0f7d33ce169bbc6a11647d4a0d41a327
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-26 19:10:46 +0200

    Refactor tag import

--- database/seeds/TagsSeeder.php
+++ database/seeds/TagsSeeder.php
@@ -11,11 +11,25 @@ class TagsSeeder extends Seeder
      */
     public function run()
     {
-        $categories = file_get_contents('https://www.yelp.com/developers/documentation/v3/all_category_list/categories.json');
+        $categories = collect(json_decode(file_get_contents('https://www.yelp.com/developers/documentation/v3/all_category_list/categories.json')));
 
-        collect(json_decode($categories))->filter(function ($category) {
+        $topCategories = $categories->filter(function ($category) {
             return collect($category->parents)->contains('shopping');
-        })->each(function ($category) {
+        });
+
+        $middleCategories = $categories->filter(function ($category) use ($topCategories) {
+            return collect($category->parents)->intersect($topCategories->pluck('alias'))->count();
+        });
+
+        $childCategories = $categories->filter(function ($category) use ($middleCategories) {
+            return collect($category->parents)->intersect($middleCategories->pluck('alias'))->count();
+        });
+
+        $topCategories
+            ->merge($middleCategories)
+            ->merge($childCategories)
+            ->unique('alias')
+            ->each(function ($category) {
             \Sneefr\Models\Tag::create([
                 'alias'     => $category->alias,
                 'title'     => $category->title,
