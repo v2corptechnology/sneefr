9d0249513edeb6cc864a1c8afbeefdcc30f6eead
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-10-27 08:23:35 +0200

    FIx FQN is incorrect

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         // Very frequent calls
 
         //if ($this->app->environment('production')) {
-            $schedule->command(Commands\ImportYelpShop::class)->everyMinute();
+            $schedule->command(\Sneefr\Console\Commands\ImportYelpShop::class)->everyMinute();
         //}
 
         // Calls made every hour
