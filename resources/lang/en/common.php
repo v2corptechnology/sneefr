279e3227debced098d77b6b5eb0bc9adac87ff8c
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-11-04 00:13:33 +0100

    Move highlited shops translations to better place

--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -1,23 +1,21 @@
 <?php
 
 return [
-    'site_description' => "Buy from great local shops in your city, all in one place",
-    'cgu'              => "<a href=\":url\" title=\"Terms of use\">Terms</a>",
-    'faq'              => "<a href = \":url\" title=\"List of frequently asked questions\">FAQ</a>",
-    'locale_name'      => "en_US.utf8",
-    'currency'         => "USD",
-    'currency_symbol'  => "$",
-    'pub'       => [
-        'title'          => "Shop owner?",
-        'description'    => "Sidewalks hepls you sell online and attract more customers in your city. No fees on sales."
+    'site_description'   => "Buy from great local shops in your city, all in one place",
+    'cgu'                => "<a href=\":url\" title=\"Terms of use\">Terms</a>",
+    'faq'                => "<a href = \":url\" title=\"List of frequently asked questions\">FAQ</a>",
+    'locale_name'        => "en_US.utf8",
+    'currency'           => "USD",
+    'currency_symbol'    => "$",
+    'pub'                => [
+        'title'       => "Shop owner?",
+        'description' => "Sidewalks hepls you sell online and attract more customers in your city. No fees on sales.",
     ],
-    'trending_shops'     => "Trending shops",
-    'trending_description'  => "Selected by sneefr, for you every week",
-    'browse_by_category'  => "Browse categories",
-    'footer' => [
-        'legal_notice'  => "Legal notice",
-        'terms_condition'  => "Terms and conditions",
-        'contact'  => "Contact",
-        'about'  => "About",
+    'browse_by_category' => "Browse categories",
+    'footer'             => [
+        'legal_notice'    => "Legal notice",
+        'terms_condition' => "Terms and conditions",
+        'contact'         => "Contact",
+        'about'           => "About",
     ],
 ];
