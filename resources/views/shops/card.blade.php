0cac88d47acf1ca162ad492a543a3cd990921679
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-11-16 12:33:12 +0100

    Refactor Shop card’s css without giving a shit to item’s

--- resources/views/shops/card.blade.php
+++ resources/views/shops/card.blade.php
@@ -5,35 +5,33 @@ $coverSize = $coverSize ?? '360x250';
 list($width, $height) = explode('x', $coverSize);
 ?>
 
-<article class="card {{ $classes ?? null }}" itemscope itemtype="http://schema.org/Product">
-    <figure class="card__gallery card__gallery--vignette">
-        <a href="{{ route('shops.show', $shop) }}" title="{{ $shop->getName() }}">
-            <img class="card__image" src="{{ $shop->getCover($coverSize) }}"
-                 srcset="{{ $shop->getCover($coverSize.'@2x') }} 2x"
-                 alt="{{ $shop->getName() }}" width="{{ $width }}"
-                 height="{{ $height }}" itemprop="image">
-        </a>
+<a class="card {{ $classes ?? null }}" href="{{ route('shops.show', $shop) }}" title="{{ $shop->getName() }}">
+    <figure class="card__gallery">
+        <img class="card__image" src="{{ $shop->getCover($coverSize) }}"
+             srcset="{{ $shop->getCover($coverSize.'@2x') }} 2x"
+             alt="{{ $shop->getName() }}" width="{{ $width }}"
+             height="{{ $height }}" itemprop="image">
+        <figcaption class="card__title">{{ $shop->getName() }}</figcaption>
     </figure>
+</a>
 
-    @include('partials.avatar', ['of' => $shop, 'size' => '40x40'])
+    {{--
+        @include('partials.avatar', ['of' => $shop, 'size' => '40x40', 'noLink' => true])
 
-    <div class="card__content">
-        <h1 class="card__title" itemprop="name">
-            <a href="{{ route('shops.show', $shop) }}"
-               title="{{ $shop->getName() }}"
-               itemprop="url">{{ $shop->getName() }}</a>
-        </h1>
+        <div class="card__content">
 
-        @if (isset($description) && $description)
-            <div class="card__text" itemprop="description">
-                <p>{{ $description }}</p>
-            </div>
-        @endif
+            <h1 itemprop="name"></h1>
 
-        @if ($footer ?? null)
-            <footer class="card__footer">
-                <p>{{ ($shop->ads_count ?? $shop->ads->count()) . ' ads' }}</p>
-            </footer>
-        @endif
-    </div>
-</article>
+            @if ($description ?? null)
+                <div class="card__text" itemprop="description">
+                    <p>{{ $description }}</p>
+                </div>
+            @endif
+
+            @if ($footer ?? null)
+                <footer class="card__footer">
+                    <p>{{ ($shop->ads_count ?? $shop->ads->count()) . ' ads' }}</p>
+                </footer>
+            @endif
+        </div>
+    </article>--}}
