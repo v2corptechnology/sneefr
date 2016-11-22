65708de367acacf43f88ac3bd7120693f82cd141
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-11-22 14:47:31 +0100

    Deactivate sliders on items cards

--- resources/views/items/card.blade.php
+++ resources/views/items/card.blade.php
@@ -10,19 +10,14 @@ list($width, $height) = explode('x', $gallerySize);
 
 $images = $ad->images($gallerySize, true);
 $images2x = $ad->images($gallerySize . '@2x', true);
-$hasMultipleImages = count($images) > 1;
 
 ?>
 <a class="card" href="{{ route('ad.show', $ad) }}" title="{{ $ad->present()->title() }}">
-    <figure class="card__gallery {{ $hasMultipleImages ? 'js-slider' : null }}" id="{{ $galleryKey }}" data-slider-h="{{ $height }}" data-slider-target="#{{ $galleryKey }}">
-        @foreach ($images as $i => $image)
-            <span class="{{ !$loop->first ? 'hidden' : '' }}">
-                <img class="card__image" {{ !$loop->first ? 'data-' : null }}src="{{ $image }}"
-                     alt="{{ $ad->present()->title() }}" width="{{ $width }}"
-                     srcset="{{ $images2x[$loop->index] }} 2x"
-                     height="{{ $height }}" itemprop="image">
-            </span>
-        @endforeach
+    <figure class="card__gallery">
+        <img class="card__image"src="{{ $images[0] }}"
+             alt="{{ $ad->present()->title() }}" width="{{ $width }}"
+             srcset="{{ $images2x[0] }} 2x"
+             height="{{ $height }}" itemprop="image">
 
         <figcaption class="card__title card__title--small">
             <span class="card__price">{!! $ad->price()->formatted() !!}</span>
