c2ed8d659d1aa1153b686f5f336313b13b0723b9
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-11-22 13:16:52 +0100

    Launch slider only if more than 1 image

--- resources/views/items/card.blade.php
+++ resources/views/items/card.blade.php
@@ -10,22 +10,11 @@ list($width, $height) = explode('x', $gallerySize);
 
 $images = $ad->images($gallerySize, true);
 $images2x = $ad->images($gallerySize . '@2x', true);
-
-switch ($detail ?? null) {
-    case 'proximity':
-        $footer = '<i class="fa fa-map-marker"></i> ' . $ad->present()->distance() . ' &mdash; ' . $ad->location();
-        break;
-    case 'evaluation':
-        $footer = trans_choice('ad.show.evaluations_ratio', $ad->present()->evaluationRatio(), ['ratio' => $ad->present()->evaluationRatio()]);
-        break;
-    case 'date':
-        $footer = HTML::time($ad->created_at);
-        break;
-}
+$hasMultipleImages = count($images) > 1;
 
 ?>
 <a class="card" href="{{ route('ad.show', $ad) }}" title="{{ $ad->present()->title() }}">
-    <figure class="card__gallery js-slider" id="{{ $galleryKey }}" data-slider-h="{{ $height }}" data-slider-target="#{{ $galleryKey }}">
+    <figure class="card__gallery {{ $hasMultipleImages ? 'js-slider' : null }}" id="{{ $galleryKey }}" data-slider-h="{{ $height }}" data-slider-target="#{{ $galleryKey }}">
         @foreach ($images as $i => $image)
             <span class="{{ !$loop->first ? 'hidden' : '' }}">
                 <img class="card__image" {{ !$loop->first ? 'data-' : null }}src="{{ $image }}"
