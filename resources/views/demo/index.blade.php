cb382f9379cb75c3cea171b72f5916a59a450ac4
Romain Sauvaire <romain.sauvaire@gmail.com>
2016-12-13 09:37:12 +0100

    Add iems to demo

--- resources/views/demo/index.blade.php
+++ resources/views/demo/index.blade.php
@@ -60,9 +60,58 @@
                 },
                 tooltip: 'RAYMOND SNEAKER SUEDE',
                 content: document.getElementById('id3').innerHTML
+            },{
+                id: 'id4',
+                polygon_px: [[2261,1678], [2330,1630], [2541,1618], [2568,1641], [2604,1640], [2758,1764], [2923,1814], [2186,1909]],
+                svgStyle: {
+                    fill: 'rgba(0, 125, 195, 0.2)',
+                    stroke: 'rgba(0, 125, 195, 0.8)',
+                    'stroke-width': '2px'
+                },
+                tooltip: 'BRAEBURN FLANNEL SHIRT',
+                content: document.getElementById('id4').innerHTML
+            },{
+                id: 'id5',
+                polygon_px: [[754,1310], [820,1342], [814,1281], [839,1251], [814,1133], [792,1116], [770,1118], [791,1134], [802,1287], [758,1284]],
+                svgStyle: {
+                    fill: 'rgba(0, 125, 195, 0.2)',
+                    stroke: 'rgba(0, 125, 195, 0.8)',
+                    'stroke-width': '2px'
+                },
+                tooltip: 'MONARCH SWEATER DRESS',
+                content: document.getElementById('id5').innerHTML
+            },{
+                id: 'id6',
+                polygon_px: [[77,1103], [53,1157], [40,1378], [70,1370], [83,1231], [119,1125], [122,1106]],
+                svgStyle: {
+                    fill: 'rgba(0, 125, 195, 0.2)',
+                    stroke: 'rgba(0, 125, 195, 0.8)',
+                    'stroke-width': '2px'
+                },
+                tooltip: 'NORDIC CARDIGAN',
+                content: document.getElementById('id6').innerHTML
+            },{
+                id: 'id7',
+                polygon_px: [[442,1143], [480,1150], [503,1135], [530,1162], [549,1332], [525,1332], [513,1349], [483,1334], [484,1315], [438,1285], [427,1272], [406,1271], [415,1213], [432,1147]],
+                svgStyle: {
+                    fill: 'rgba(0, 125, 195, 0.2)',
+                    stroke: 'rgba(0, 125, 195, 0.8)',
+                    'stroke-width': '2px'
+                },
+                tooltip: 'ROYAL OAK SWEATER',
+                content: document.getElementById('id7').innerHTML
+            },{
+                id: 'id8',
+                polygon_px: [[1984,1468], [2019,1487], [2065,1480], [2107,1504], [2163,1498], [2182,1444], [2196,1393], [2161,1366], [2117,1368], [2099,1355], [2061,1364], [2017,1367], [2002,1409]],
+                svgStyle: {
+                    fill: 'rgba(0, 125, 195, 0.2)',
+                    stroke: 'rgba(0, 125, 195, 0.8)',
+                    'stroke-width': '2px'
+                },
+                tooltip: 'MENS CHUKKA BOOT SUEDE',
+                content: document.getElementById('id8').innerHTML
             }]
         });
-
 
         var poly = '';
         PSV.on('click', function (e) {
@@ -247,6 +296,253 @@
             </div>
         </div>
     </div>
+
+
+    <div id="id4">
+        <div class="box">
+
+            <h1 class="item__heading">BRAEBURN FLANNEL SHIRT</h1>
+
+            <img class="ad__images__preview img-responsive" src="https://eazkmue.cloudimg.io/crop/500x500/q75.tjpg/_originals_/1249/de2630fff98eef288737a65ff0e87438.jpg" alt="">
+
+            <div class="item__meta">
+                <span class="item__stock">4 in stock</span>
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__buttons">
+
+                <del class="item__tagprice text-muted">$78.00</del>
+
+                <span class="item__price">$58.99</span>
+
+                <div class="btn-group btn-group-lg pull-right">
+
+                    <a href="#LoginBefore" data-toggle="modal" title="" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+
+                        <li><a href="#reportAd" title="" data-toggle="modal"><i class="fa fa-fw fa-warning"></i> Report item</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__social">
+                <a href="https://www.sidewalks.city/share/ad/1249-braeburn-flannel-shirt" class="btn btn-link" title="" data-toggle="modal" data-remote="false" data-target="#shareModal">
+                    <i class="icon fa fa-lg fa-share-alt"></i> Share
+                </a>
+
+                <a href="#writeTo" class="btn btn-link" title="" data-toggle="modal" data-remote="false">
+                    <i class="icon fa fa-lg fa-comment-o"></i> Contact
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <div id="id5">
+        <div class="box">
+
+            <h1 class="item__heading">MONARCH SWEATER DRESS</h1>
+
+            <img class="ad__images__preview img-responsive" src="https://eazkmue.cloudimg.io/crop/500x500/q75.tjpg/_originals_/1247/34400f4d7b11d71b64b554223fd9ca7d.jpg" alt="">
+
+            <div class="item__meta">
+                <span class="item__stock">4 in stock</span>
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__buttons">
+
+                <del class="item__tagprice text-muted">$142.00</del>
+
+                <span class="item__price">$108.99</span>
+
+                <div class="btn-group btn-group-lg pull-right">
+
+                    <a href="#LoginBefore" data-toggle="modal" title="" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+
+                        <li><a href="#reportAd" title="" data-toggle="modal"><i class="fa fa-fw fa-warning"></i> Report item</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__social">
+                <a href="https://www.sidewalks.city/share/ad/1249-braeburn-flannel-shirt" class="btn btn-link" title="" data-toggle="modal" data-remote="false" data-target="#shareModal">
+                    <i class="icon fa fa-lg fa-share-alt"></i> Share
+                </a>
+
+                <a href="#writeTo" class="btn btn-link" title="" data-toggle="modal" data-remote="false">
+                    <i class="icon fa fa-lg fa-comment-o"></i> Contact
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="id7">
+        <div class="box">
+
+            <h1 class="item__heading">ROYAL OAK SWEATER</h1>
+
+            <img class="ad__images__preview img-responsive" src="https://eazkmue.cloudimg.io/crop/500x500/q75.tjpg/_originals_/1245/5a63f5dc35fdc99f1a3017a84f1872d8.jpg" alt="">
+
+            <div class="item__meta">
+                <span class="item__stock">3 in stock</span>
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__buttons">
+
+                <span class="item__price">$88.00</span>
+
+                <div class="btn-group btn-group-lg pull-right">
+
+                    <a href="#LoginBefore" data-toggle="modal" title="" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+
+                        <li><a href="#reportAd" title="" data-toggle="modal"><i class="fa fa-fw fa-warning"></i> Report item</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__social">
+                <a href="https://www.sidewalks.city/share/ad/1249-braeburn-flannel-shirt" class="btn btn-link" title="" data-toggle="modal" data-remote="false" data-target="#shareModal">
+                    <i class="icon fa fa-lg fa-share-alt"></i> Share
+                </a>
+
+                <a href="#writeTo" class="btn btn-link" title="" data-toggle="modal" data-remote="false">
+                    <i class="icon fa fa-lg fa-comment-o"></i> Contact
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div id="id6">
+        <div class="box">
+
+            <h1 class="item__heading">NORDIC CARDIGAN</h1>
+
+            <img class="ad__images__preview img-responsive" src="https://eazkmue.cloudimg.io/crop/500x500/q75.tjpg/_originals_/1246/fb0045dbeca28f95b2cfc404123bbbcd.jpg" alt="">
+
+            <div class="item__meta">
+                <span class="item__stock">3 in stock</span>
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__buttons">
+
+                <del class="item__tagprice text-muted">$116.00</del>
+
+                <span class="item__price">$86.99</span>
+
+                <div class="btn-group btn-group-lg pull-right">
+
+                    <a href="#LoginBefore" data-toggle="modal" title="" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+
+                        <li><a href="#reportAd" title="" data-toggle="modal"><i class="fa fa-fw fa-warning"></i> Report item</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__social">
+                <a href="https://www.sidewalks.city/share/ad/1249-braeburn-flannel-shirt" class="btn btn-link" title="" data-toggle="modal" data-remote="false" data-target="#shareModal">
+                    <i class="icon fa fa-lg fa-share-alt"></i> Share
+                </a>
+
+                <a href="#writeTo" class="btn btn-link" title="" data-toggle="modal" data-remote="false">
+                    <i class="icon fa fa-lg fa-comment-o"></i> Contact
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div id="id8">
+        <div class="box">
+
+            <h1 class="item__heading">MENS CHUKKA BOOT SUEDE</h1>
+
+            <img class="ad__images__preview img-responsive" src="https://eazkmue.cloudimg.io/crop/500x500/q75.tjpg/_originals_/1252/13c87ac3f8c3ad590e5f9953c872fbcf.JPG" alt="">
+
+            <div class="item__meta">
+                <span class="item__stock">3 in stock</span>
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__buttons">
+
+                <span class="item__price">$176.99</span>
+
+                <div class="btn-group btn-group-lg pull-right">
+
+                    <a href="#LoginBefore" data-toggle="modal" title="" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+
+                        <li><a href="#reportAd" title="" data-toggle="modal"><i class="fa fa-fw fa-warning"></i> Report item</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <hr class="item__separator">
+
+            <div class="item__social">
+                <a href="https://www.sidewalks.city/share/ad/1249-braeburn-flannel-shirt" class="btn btn-link" title="" data-toggle="modal" data-remote="false" data-target="#shareModal">
+                    <i class="icon fa fa-lg fa-share-alt"></i> Share
+                </a>
+
+                <a href="#writeTo" class="btn btn-link" title="" data-toggle="modal" data-remote="false">
+                    <i class="icon fa fa-lg fa-comment-o"></i> Contact
+                </a>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 
@@ -276,7 +572,7 @@
                     <h2 class="summary__head">
                         <i class="fa fa-globe summary__icon"></i>
                         <a href="https://www.sidewalks.city/shops/roots-beverly-hills-beverly-hills">
-                            7 ads
+                            8 ads
                         </a>
                     </h2>
                     <p class="summary__content summary__content--extra">
@@ -294,16 +590,10 @@
                     </h2>
                 </li>
 
-                <li>
-                    <a class="btn btn-block btn-primary btn-primary2" href="https://www.sidewalks.city/shops/roots-beverly-hills-beverly-hills/edit" title="___">
-                        <i class="fa fa-cog"></i>
-                        Edit
-                    </a>
-                </li>
 
                 <li>
                     <form action="https://www.sidewalks.city/claims" method="post">
-                        <input name="_token" value="HQ0dELYISSFkCIsDPuAERIS0aWZQTsKUU46L8OCC" type="hidden">
+                        <input name="_token" value="VRLvENqUkUh3MoQofgoOUTapheuTy7yo6JUxbm4x" type="hidden">
                         <input name="shop_id" value="26060" type="hidden">
                         <button class="btn btn-block btn-primary btn-primary2" type="submit">
                             <i class="fa fa-handshake-o"></i>
@@ -321,7 +611,7 @@
 
                 <div class="col-sm-8">
                     <h1 class="content-head" id="common">
-                        7 ads at Roots - Beverly Hills            </h1>
+                        8 ads at Roots - Beverly Hills            </h1>
                 </div>
 
 
@@ -346,6 +636,18 @@
 
 
             <div class="row">
+                <div class="col-md-6">
+                    <a class="card" href="https://www.sidewalks.city/ad/1252-mens-chukka-boot-suede" title="MENS CHUKKA BOOT SUEDE">
+                        <figure class="card__gallery">
+                            <img class="card__image" src="https://eazkmue.cloudimg.io/crop/360x250/q75.tjpg/_originals_/1252/13c87ac3f8c3ad590e5f9953c872fbcf.JPG" alt="MENS CHUKKA BOOT SUEDE" srcset="https://eazkmue.cloudimg.io/crop/720x500/q75.tjpg/_originals_/1252/13c87ac3f8c3ad590e5f9953c872fbcf.JPG 2x" itemprop="image" width="360" height="250">
+
+                            <figcaption class="card__title card__title--small">
+                                <span class="card__price">$176.00</span>
+                                MENS CHUKKA BOOT SUEDE
+                            </figcaption>
+                        </figure>
+                    </a>
+                </div>
                 <div class="col-md-6">
                     <a class="card" href="https://www.sidewalks.city/ad/1251-raymond-sneaker-suede" title="RAYMOND SNEAKER SUEDE">
                         <figure class="card__gallery">
